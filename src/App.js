2f52157536da24b7560b516e3098c15288e7a653
Mick <mhearne@redhat.com>
2019-11-06 22:09:01 +0000

    breaking out from app.js

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import {Platform} from 'react-native';
+import {createAppContainer} from 'react-navigation';
+import {createBottomTabNavigator} from 'react-navigation-tabs';
+import {createMaterialBottomTabNavigator} from 'react-navigation-material-bottom-tabs';
+import tabScreenConfig from './navigation';
+
+const TabNavigator = Platform.OS === "android" ?
+  createMaterialBottomTabNavigator(tabScreenConfig)
+  : createBottomTabNavigator(tabScreenConfig);
+
+export default createAppContainer(TabNavigator);
---
+++ src/navigation.js
@@ -0,0 +1,15 @@
+import HomeScreen from './components/HomeScreen';
+import SecondScreen from './components/SecondScreen';
+import ThirdScreen from './components/ThirdScreen';
+
+export default tabScreenConfig = {
+  Home: {
+    screen: HomeScreen,
+  },
+  Second: {
+    screen: SecondScreen,
+  },
+  Third: {
+    screen: ThirdScreen,
+  }
+};
