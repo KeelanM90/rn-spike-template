a3c21a054652a2834febd4bc9ea4d131538bbc0f
Mick <mhearne@redhat.com>
2019-11-07 09:55:44 +0000

    adding config

--- src/App.js
+++ src/App.js
@@ -2,10 +2,12 @@ import {Platform} from 'react-native';
 import {createAppContainer} from 'react-navigation';
 import {createBottomTabNavigator} from 'react-navigation-tabs';
 import {createMaterialBottomTabNavigator} from 'react-navigation-material-bottom-tabs';
-import tabScreenConfig from './navigation/';
+import tabScreenConfig from './navigation';
 
 const TabNavigator = Platform.OS === "android" ?
   createMaterialBottomTabNavigator(tabScreenConfig)
   : createBottomTabNavigator(tabScreenConfig);
 
-export default createAppContainer(TabNavigator);
+const App = createAppContainer(TabNavigator);
+
+export default App;
